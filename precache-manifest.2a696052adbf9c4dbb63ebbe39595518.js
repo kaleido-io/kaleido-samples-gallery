5952026146aa9f806b480683c6f2eb773999c53c
jebonfig <joseph.bonfiglio@consensys.net>
2018-10-24 16:00:18 -0400

    Updates

---
+++ precache-manifest.2a696052adbf9c4dbb63ebbe39595518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3f756db637d00f4ddbf",
+    "url": "/kaleido-samples/static/js/runtime~main.d3f756db.js"
+  },
+  {
+    "revision": "3a38df1023114b5853ef",
+    "url": "/kaleido-samples/static/js/main.3a38df10.chunk.js"
+  },
+  {
+    "revision": "bc875f50e846efd05df5",
+    "url": "/kaleido-samples/static/js/1.bc875f50.chunk.js"
+  },
+  {
+    "revision": "3a38df1023114b5853ef",
+    "url": "/kaleido-samples/static/css/main.08d0a01d.chunk.css"
+  },
+  {
+    "revision": "754b469b4dd90d21017037513d9d244a",
+    "url": "/kaleido-samples/index.html"
+  }
+];
