36ffbb4c66b6a6f861cc8aaac6e7c491736008fb
jebonfig <joseph.bonfiglio@consensys.net>
2018-11-17 19:55:46 -0500

    Updates

--- precache-manifest.78f47daf964ede5913e30444f85e24c8.js
+++ precache-manifest.78f47daf964ede5913e30444f85e24c8.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/kaleido-samples-gallery/static/js/runtime~main.38bc574b.js"
   },
   {
-    "revision": "fc599028f4da64de4588",
-    "url": "/kaleido-samples-gallery/static/js/main.fc599028.chunk.js"
+    "revision": "7d91771cefc6a8108aa1",
+    "url": "/kaleido-samples-gallery/static/js/main.7d91771c.chunk.js"
   },
   {
     "revision": "580969555bec1817518c",
     "url": "/kaleido-samples-gallery/static/js/1.58096955.chunk.js"
   },
   {
-    "revision": "fc599028f4da64de4588",
-    "url": "/kaleido-samples-gallery/static/css/main.8439825c.chunk.css"
+    "revision": "7d91771cefc6a8108aa1",
+    "url": "/kaleido-samples-gallery/static/css/main.ad7e9adb.chunk.css"
   },
   {
-    "revision": "66dd7b31889a65d155eddd51e26acd74",
+    "revision": "7427861a8708edecad81ba176d848a97",
     "url": "/kaleido-samples-gallery/index.html"
   }
 ];
